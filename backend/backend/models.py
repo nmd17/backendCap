5d7082cb7f3579f05833171a6820faf7e2fd3ecd
Nick D <nmdanner123@gmail.com>
2019-10-12 06:23:21 -0400

    conference model bug fix

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Conference(models.Model):
+    conference_ministers = models.ManyToManyField(User)
     name = models.CharField(max_length=150)
+    location = models.CharField(max_length=100)
